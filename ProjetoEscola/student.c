266ca56ee2f2d13241bdf950347ab3f3eddac23b
Alexandre Morais <102628698+Ednaxx@users.noreply.github.com>
2024-02-28 21:52:30 +0000

    Fixed GetAllStudents and implemented findById

--- ProjetoEscola/student.c
+++ ProjetoEscola/student.c
@@ -7,12 +7,6 @@
 Person students[MAX_RECORDS];
 int studentAmount = 0;
 
-int generateID() {
-  static long num = 0;
-  num++;
-  return num;
-}
-
 void createStudent() {
   Person newStudent;
   Date newBirthday;
@@ -30,7 +24,9 @@ void createStudent() {
   scanf("%d/%d/%d", &newBirthday.day, &newBirthday.month, &newBirthday.year);
   newStudent.birthday = newBirthday;
 
-  newStudent.id = generateID();
+  newStudent.id = studentAmount;
+
+  newStudent.active = 1;
 
   students[studentAmount] = newStudent;
   studentAmount++;
@@ -41,18 +37,26 @@ void createStudent() {
 void getAllStudents() {
   puts("\nMatrícula - Nome do aluno");
   for (int i = 0; i < studentAmount; i++) {
-    printf("%ld - %s", students[i].id, students[i].name);
+    if (students[i].active == 1) printf("%ld - %s\n", students[i].id, students[i].name);
   }
-  puts("");
 }
 
 void getStudent() {
-  long input;
+  long id;
   
   puts("\nInsira a matrícula do aluno:");
-  scanf("%ld", &input);
+  scanf("%ld", &id);
 
-  // TODO: Look for data
+  for (int i = 0; i < studentAmount; i++) {
+    if (students[i].id != id && students[i].active == 1) break;
+
+    printf("Matrícula: %ld\n", students[i].id);
+    printf("Nome: %s\n", students[i].name);
+    printf("Sexo: %s\n", (students[i].gender == 0) ? "Masculino" : "Feminino");
+    printf("CPF: %ld\n", students[i].CPF);
+    printf("Aniversário: %d/%d/%d\n", students[i].birthday.day, students[i].birthday.month, students[i].birthday.year);
+    break;
+  }
   
 }
 
